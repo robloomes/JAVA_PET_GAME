7d9fabb1989e9aba2a6557e6d79113e421e84fcc
robloomes <robloomes@gmail.com>
2017-04-12 12:44:24 +1200

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,15 +1,16 @@
 package pet_game;
+
 public class Food {
 	
-	
+	//Initializations
 	String name;
 	int price;
 	float weight;
 	int tastiness;
 	int nutrition;
 	
-	
-	Food(String name,int price, float weight, int tastiness, int nutrition){
+	//Constructor
+	Food(String name, int price, float weight, int tastiness, int nutrition) {
 		this.name = name;
 		this.price = price;
 		this.weight = weight;
@@ -17,51 +18,42 @@ public class Food {
 		this.nutrition = nutrition;
 	}
 	
-
-	
-	// getter
-	
-	public String getname(){
-	return name;
+	//Getters
+	public String getname() {
+		return name;
 	}
 	
-	public int getPrice(){
+	public int getPrice() {
 		return price;
 	}
 	
-	public float getWeight(){
+	public float getWeight() {
 		return weight;
 	}
 	
-	public int gettestines(){
+	public int gettestines() {
 		return tastiness;
 	}
 	
-	public void setnutrition(int newNutrition){
+	public void setNutrition(int newNutrition) { //TODO is this method needed?
 	this.nutrition = newNutrition ;
-		
 	}
-	
-
-
 	
 	public String toString() {
 		 String foodName = "Name: " + this.name;
 		 String foodPrice = "Price: " + this.price;
-		 String foodWeight = "weight: " + this.weight;
-		 String foodTastiness = "tastiness: " + this.tastiness;
-		 String foodNutrition = "nutrition: " + this.nutrition;
+		 String foodWeight = "Weight: " + this.weight;
+		 String foodTastiness = "Tastiness: " + this.tastiness;
+		 String foodNutrition = "Nutrition: " + this.nutrition;
 		 
-		 
-		 String foodStats = foodName + "\n" + foodPrice + "\n" + foodWeight + "\n" + foodNutrition + "\n" + foodTastiness ;
+		 String foodStats = foodName + "\n" + foodPrice + "\n" + foodWeight + "\n" + foodNutrition + "\n" + foodTastiness;
 		 return foodStats;
 	}
+	
 	public static void main(String[]args){
 		//unit testing
-		Food testTuna = new Food("toby", 18, 100, 7, 12 ); //(name, price, weight, nutrition , tastiness )
+		Food testTuna = new Food("Tuna", 18, 100, 7, 12 ); //(name, price, weight, nutrition , tastiness)
 		System.out.println(testTuna);
 	}
-
-
 
 }
