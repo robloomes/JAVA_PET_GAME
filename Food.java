333e718ba78f4beaad3807e5aab15f0c7004facf
robloomes <robloomes@gmail.com>
2017-05-23 22:15:33 +1200

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,15 +1,35 @@
 package pet_game;
-
+/**
+ * Food.java - a class used for building Food objects.
+ * <p>
+ * This contains methods for returning and changing Food attributes
+ * Food objects created are mainly used in association with classes @see Pet and @see gameEnvironment. 
+ * 
+ * @author Robert Loomes
+ * @author Mna Chalabi
+ * @since Winter 2017
+ * @version 1.0
+ */
 public class Food {
 	
-	//Initializations
+	/**
+	 * Initialize the variables of the class
+	 */
 	String name;
 	int price;
 	String size;
 	int tastiness;
 	int nutrition;
 	
-	//Constructor
+	/**
+	 * Constructor method for Food class.
+	 * @param name A variable of type String.
+	 * @param price A variable of type Integer.
+	 * @param size A variable of type String.
+	 * @param tastiness A variable of type Integer
+	 * @param nutrition A variable of type Integer.
+	 * @return Nothing.
+	 */
 	Food(String name, int price, String size, int tastiness, int nutrition) {
 		this.name = name;
 		this.price = price;
@@ -18,30 +38,59 @@ public class Food {
 		this.nutrition = nutrition;
 	}
 	
-	//Getters
+	/**
+	 * Getter method
+	 * @return This returns the String name variable.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the int Price variable.
+	 */
 	public int getPrice() {
 		return price;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the String size variable.
+	 */
 	public String getSize() {
 		return size;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the int tastiness variable.
+	 */
 	public int getTastiness() {
 		return tastiness;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the int nutrition variable.
+	 */
 	public int getNutrition(){
 		return nutrition;
 	}
-	public void setNutrition(int newNutrition) { //TODO is this method needed?
+	
+	/**
+	 * Sets the int nutrition variable to a new value.
+	 * @param newNutrition this is the new variable for int nutrition.
+	 */
+	public void setNutrition(int newNutrition) { 
 		this.nutrition = newNutrition ;
 	}
 	
+	/**
+	 * Overides the default toString method.
+	 * Produces a formatted String of all Food variables.
+	 * @return Formatted String.
+	 */
 	public String toString() {
 		String foodName = "Name: " + this.name;
 		String foodPrice = "Price: " + this.price;
@@ -52,11 +101,4 @@ public class Food {
 		String result = String.format("%s %n%s %n%s %n%s %n%s", foodName, foodPrice, foodSize, foodTastiness, foodNutrition);
 		return result;
 	}
-	
-	public static void main(String[]args){
-		//unit testing
-		Food testTuna = new Food("Tuna", 18, "large", 7, 12 ); //(name, price, size, nutrition , tastiness)
-		System.out.println(testTuna);
-	}
-
 }
