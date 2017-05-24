b48ef59164332bf51c1cffdfd0724434fd7ddd9b
mnachalabi <mkc49@uclive.ac.nz>
2017-05-24 22:20:05 +1200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,9 +1,19 @@
 package pet_game;
 import java.util.*;
 
+/**
+ * Player.java - a class used for building Player objects. 
+ * Food objects created are mainly used in association with classes  @see gameEnvironment.
+ * @author Robert Loomes
+ * @author Mna Chalabi
+ * @since Winter 2017
+ * @version 1.0
+ */
 public class Player {
 	
-	//Initializations
+	/**
+	 * Initialize the variables of the class
+	 */
 	
 	static Set<String> playerSet = new HashSet<String>();
 	String name;
@@ -12,8 +22,10 @@ public class Player {
 	private Map<Toy, Integer> ownedToys = new HashMap<Toy, Integer>();
 	private Map<Food, Integer> ownedFood = new HashMap<Food, Integer>();
 
-	
-	//Constructor
+	 /** Constructor method for Food class.
+	 * @param name A variable of type String.
+	 * @return Nothing.
+	 */
 	Player(String name){
 		this.name = name;
 		playerSet.add(name);
@@ -21,60 +33,103 @@ public class Player {
 
 	}
 	
-	//Getters
+	/**
+	 * Getter method
+	 * @return This returns the String name variable.
+	 */
 	public String getName() {
 		return name;
 	}
-	
+	/**
+	 * Getter method
+	 * @return This returns the ownedPets list.
+	 */
 	public Map<String, Species> getPets() {
 		return ownedPets;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the ownedToys list.
+	 */
 	public Map<Toy, Integer> getToys() {
 		return ownedToys;
 	}
 	
-	
+	/**
+	 * Getter method
+	 * @return This returns the ownedFood list.
+	 */	
 	public Map<Food, Integer> getFood() {
 		return ownedFood;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the Integer playerBalance variable.
+	 */
 	public int getBalance() {
 		return playerBalance;
 	}
 	
-	
-	//setters
+	/**
+	 * Sets the int newBalance variable to a new value.
+	 * @param newBalance this is the new variable for int Balance.
+	 */
 	public void setBalance(int newBalance){
 		this.playerBalance = newBalance; 
 	}
+	
+	/**
+	 * addPet is Method that adds pet to the list of the player.
+	 * @param pet to sets a name for the pet.
+	 */
 	public void addPet(Species pet) {
 		ownedPets.put(pet.getName(), pet);
 	}
 	
+	/**
+	 * removePet is Method that remove pet from the list of the player.
+	 */
 	public void removePet(Pet pet) {
 		ownedPets.remove(pet);
 		ownedPets.remove(pet, 0);
 	}
 	
+	/**
+	 * addToy is Method that adds Toys to the list of the player.
+	 */
 	public void addToy(Toy toy) {
 		ownedToys.merge(toy, 1, Integer::sum);
 	}
 	
+	/**
+	 * removePet is Method that remove Toys from the list of the player.
+	 */
 	public void removeToy(Toy toy) {
 		ownedToys.merge(toy, -1, Integer::sum);
 		ownedToys.remove(toy, 0);
 	}
 	
+	/**
+	 * addFood is Method that adds Foods to the list of the player.
+	 */
 	public void addFood(Food food) {
 		ownedFood.merge(food, 1, Integer::sum);
 	}
-	
+	/**
+	 * removeFood is Method that remove Food from the list of the player.
+	 */
 	public void removeFood(Food food) {
 		ownedFood.merge(food, -1, Integer::sum);
 		ownedFood.remove(food, 0);
 	}
 
+	/**
+	 * Overides the default toString method.
+	 * Produces a formatted String of all Player variables.
+	 * @return Formatted String.
+	 */
 	public String toString() {
 		String playerName = "Player name: " + name;
 		String balanceString = "Balance: $" + playerBalance;
