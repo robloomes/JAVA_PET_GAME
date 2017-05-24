d144dba53a1d5b1b1e8c7a95cedfbc86aa00312e
mnachalabi <mkc49@uclive.ac.nz>
2017-05-24 21:18:35 +1200

    Update Toy.java

--- Toy.java
+++ Toy.java
@@ -1,13 +1,33 @@
 package pet_game;
+/**
+ * Toy.java - a class used for building Toy objects.
+ * <p>
+ * This contains methods for returning and changing Toy attributes
+ * Toy objects created are mainly used in association with classes @see Pet and @see gameEnvironment. 
+ * 
+ * @author Robert Loomes
+ * @author Mna Chalabi
+ * @since Winter 2017
+ * @version 1.0
+ */
 public class Toy {
 	
-	//Initializations
+	/**
+	 * Initialize the variables of the class
+	 */
 	String name;
 	int price;
 	int durability;
 	int enjoyment;
 	
-	//Constructor for Toy class
+	/**
+	 * Constructor method for Food class.
+	 * @param name A variable of type String.
+	 * @param price A variable of type Integer
+	 * @param durability A variable of type Integer.
+	 * @param enjoyment A variable of type Integer.
+	 * @return Nothing.
+	 */
 	Toy(String name, int price, int durability, int enjoyment) {
 		this.name = name;
 		this.price = price;
@@ -15,7 +35,12 @@ public class Toy {
 		this.enjoyment = enjoyment;
 	}	
 	
-	//Constructor for clone class
+	/**
+	 * Constructor method for clone class.
+	 * create unique variavble for each pets.
+	 * @param name A variable of type String.
+	 * @return Nothing.
+	 */
 	Toy(Toy toy) {
 		this.name = toy.getName();
 		this.price = toy.getPrice();
@@ -23,30 +48,52 @@ public class Toy {
 		this.enjoyment = toy.getEnjoyment();
 	}	
 	
-	//Getters
+	/**
+	 * Getter method
+	 * @return This returns the String name variable.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the integer price variable.
+	 */
 	public int getPrice() {
 		return price;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the integer durability variable.
+	 */
 	public int getDurability() {
 		return durability;
 	}
 	
+	/**
+	 * Getter method
+	 * @return This returns the integer enjoyment variable.
+	 */
 	public int getEnjoyment() {
 		return enjoyment;
 	}
 	
-	//Setters
+	/**
+	 * Sets the int newDurability variable to a new value.
+	 * @param newDurability this is the new variable for int Durability.
+	 */
 	public void setDurability(int newDurability) {
 		this.durability = newDurability;
 	}
 	
+	/**
+	 * Overides the default toString method.
+	 * Produces a formatted String of all Pet variables.
+	 * @return Formatted String.
+	 */
 	public String toString() {
-		//Output format
 		String toyName = "Name: " + this.name;
 		String toyPrice = "Price: " + this.price;
 		String toyDurability = "Durability: " + this.durability;
@@ -56,8 +103,8 @@ public class Toy {
 		return result;
 	}
 	
+	
 	public static void main(String[]args) {
-		//Unit testing
 		Toy testBall = new Toy("Ball", 18, 100, 7); //(name, price, durability, enjoyment)
 		System.out.println(testBall);
 
