9cbbd0dae1e51071b7c91eb10c2e2a5caa13ce78
robloomes <robloomes@gmail.com>
2017-06-02 00:39:58 +1200

    Update ToyTest.java

--- ToyTest.java
+++ ToyTest.java
@@ -1,22 +1,21 @@
 package pet_game;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class ToyTest {
-	
+
 	@Test
 	public void testSetDurability() {
-		Toy test = new Toy("tuna", 10,2,3);
-		assertEquals(2,test.getDurability());
-		}
-	
+		Toy test = new Toy("tuna", 10, 2, 3);
+		assertEquals(2, test.getDurability());
+	}
+
 	@Test
 	public void testToString() {
-		Toy test = new Toy("Yoyo",10,2,3);
+		Toy test = new Toy("Yoyo", 10, 2, 3);
 		assertEquals("Name: Yoyo \nPrice: 10 \nDurability: 2 \nEnjoyment: 3", test.toString());
 	}
-	
 
 }
